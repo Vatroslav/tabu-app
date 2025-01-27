eb3ec7b97cc93fdf216dec6137f0fbe21f65bf0e
Renato Juras <renato.juras@boxmark.com>
2025-01-27 13:30:54 +0100

    vite

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    https: true,
+    host: 'localhost',
+    port: 8080,
+    hmr: {
+      protocol: 'wss',
+      host: 'localhost',
+      port: 8080
+    }
+  }
+})
