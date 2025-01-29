1bf2c25690f3363881c60f094c8904676119e495
Vatroslav Mileusnić <mileusnic.vatroslav@gmail.com>
2025-01-29 16:08:38 +0100

    Fixed CORS and included https support. Note: you must create your own /certs/server.key and /certs/server.cert files

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
+
 const app = express();
 
-app.use(cors());
+const options = {
+  key: fs.readFileSync('./certs/server.key'),
+  cert: fs.readFileSync('./certs/server.cert')
+};
+
+app.use(cors({
+    origin: 'https://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 app.use('/src', express.static(path.join(__dirname, 'src')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/endpoint', (req, res) => {
-  res.json({ message: 'This is CORS-enabled for all origins!' });
+    res.json({ message: 'This is HTTPS-enabled!' });
 });
 
-app.listen(3000, () => {
-  console.log('CORS-enabled web server listening on port 3000');
+https.createServer(options, app).listen(3000, () => {
+    console.log('HTTPS server running on https://localhost:3000');
 });
