685503498d17f1500554b764a4be416f81e4863c
Vatroslav Mileusnić <mileusnic.vatroslav@gmail.com>
2025-01-29 18:02:55 +0100

    De-duplicated CSS

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -42,107 +42,92 @@ export default {
   padding-left: 20px;
 }
 
-@media (min-width: 768px) {
+h1 {
+  font-weight: bold;
+  color: #333333;
+  position: relative;
+  display: inline-block;
+  margin-bottom: 50px;
+}
+
+h1::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: -1;
+  background-color: #D9FF80;
+}
+
+.salary-container {
+  display: flex;
+  flex-direction: column;
+  width: fit-content; /* Container width becomes text size */
+}
+  
+.label {
+  font-weight: bold;
+  color: #333333;
+  margin-bottom: 5px;
+  margin: 0;
+  line-height: 1;
+}
+
+.salary {
+  font-weight: bold;
+  color: #FF9883;
+  margin: 0;
+  line-height: 1;
+}
+
+.salary-label {
+  color: #333333;
+  margin-top: 5px;
+  text-align: right; /* Aligns right within the container */
+}
+
+@media (min-width: 768px) { /* Large screens */
   h1 {
     font-size: 48px;
-    font-weight: bold;
-    color: #333333;
-    position: relative;
-    display: inline-block;
-    margin-bottom: 50px;
   }
 
   h1::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
     height: 10px;
-    z-index: -1;
-    background-color: #D9FF80;
-  }
-
-  .salary-container {
-    display: flex;
-    flex-direction: column;
-    width: fit-content; /* Container width becomes text size */
   }
 
   .label {
     font-size: 24px;
-    font-weight: bold;
-    color: #333333;
-    margin-bottom: 5px;
-    margin: 0;
-    line-height: 1;
   }
 
   .salary {
     font-size: 72px;
-    font-weight: bold;
-    color: #FF9883;
-    margin: 0;
-    line-height: 1;
   }
 
   .salary-label {
     font-size: 18px;
-    color: #333333;
-    margin-top: 5px;
-    text-align: right; /* Aligns right within the container */
   }
 }
 
-@media (max-width: 768px) {
+@media (max-width: 768px) { /* Mobile phones */
   h1 {
     font-size: 24px;
-    font-weight: bold;
-    color: #333333;
-    position: relative;
-    display: inline-block;
-    margin-bottom: 50px;
   }
 
   h1::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
     height: 5px;
-    z-index: -1;
-    background-color: #D9FF80;
-  }
-
-  .salary-container {
-    display: flex;
-    flex-direction: column;
-    width: fit-content; /* Container width becomes text size */
   }
 
   .label {
     font-size: 24px;
-    font-weight: bold;
-    color: #333333;
-    margin-bottom: 5px;
-    margin: 0;
-    line-height: 1;
   }
 
   .salary {
     font-size: 48px;
-    font-weight: bold;
-    color: #FF9883;
-    margin: 0;
-    line-height: 1;
   }
 
   .salary-label {
     font-size: 12px;
-    color: #333333;
-    margin-top: 5px;
-    text-align: right; /* Aligns right within the container */
   }
 }
 </style>
