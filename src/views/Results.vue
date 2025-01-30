b592e892b4d5352b1a40908a76f9b0f660b098a1
Vatroslav Mileusnić <mileusnic.vatroslav@gmail.com>
2025-01-30 17:51:15 +0100

    Started working on 0.2 frontend

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -3,10 +3,85 @@
     <Navbar />
     <div class="results">
       <h1>My salary comparison</h1>
-      <div class="salary-container">
-        <p class="label">My salary</p>
-        <p class="salary">1,234 €</p>
-        <p class="salary-label">net salary</p>
+      <!----------------- Filters ----------------->
+      <div class="overall-filter-section">
+        <div class="filters">
+          <p class="filters-title">Customize the report by selecting filters:</p>
+
+          <div class="filter-row">
+            <label for="position" class="filter-label">Position:</label>
+            <select id="position" value="Sales Assistant" class="input-field">
+              <option value="my_position">Sales Assistant</option>
+              <option value="other_positions_in_department">Other positions in the department</option>
+              <option value="additional_position">Additional position: Backend</option>
+            </select>
+          </div>
+
+          <div class="filter-row seniority-row">
+            <label class="filter-label">Seniority:</label>
+            <div class="seniority-group">
+              <button class="seniority-btn">Junior</button>
+              <button class="seniority-btn active">Middle</button>
+              <button class="seniority-btn">Senior</button>
+            </div>
+          </div>
+
+          <div class="filter-row">
+            <label for="technology" class="filter-label">Technology:</label>
+            <select type="text" id="technology" value="No technology" class="input-field disabled" disabled>
+              <option value="no_technology">No technology</option>
+              <option value="technology">Technology</option>
+            </select>
+          </div>
+
+          <div class="filter-row">
+            <label for="country" class="filter-label">Country:</label>
+            <select id="country" class="input-field">
+              <option value="Bosnia and Herzegovina">Bosnia and Herzegovina</option>
+              <option value="Croatia" selected>Croatia</option>
+              <option value="Serbia">Serbia</option>
+            </select>
+          </div>
+
+          <div class="filter-row">
+            <label for="contract" class="filter-label">Contract:</label>
+            <select id="contract" class="input-field">
+              <option value="Permanent employment contract" selected>Permanent employment contract</option>
+              <option value="Fixed-term employment contract">Fixed-term employment contract</option>
+              <option value="Student contract">Student contract</option>
+              <option value="Sole proprietorship (obrt)">Sole proprietorship (obrt)</option>
+              <option value="I have my own company">I have my own company</option>
+              <option value="Contract for work">Contract for work</option>
+              <option value="Gaining initial work experience/internship">Gaining initial work experience/internship
+              </option>
+              <option value="Copyright contract">Copyright contract</option>
+            </select>
+          </div>
+        </div>
+        <!----------------- Amount of salaries ----------------->
+        <div class="salaries-amount-container">
+          <p class="salaries-amount-number" id="amount-of-salaries">176</p>
+          <p class="salaries-amount-label">salaries in this filter</p>
+          <p class="salaries-amount-note">Modify the filters to change the salaries you're compared to.</p>
+        </div>
+      </div>
+      <!----------------- Data containers ----------------->
+      <div class="overall-data-section">
+        <div class="my-salary-container">
+          <p class="data-type-label">My salary</p>
+          <p class="salary-my">1,234 €</p>
+          <p class="salary-type-label">net salary</p>
+        </div>
+        <div class="average-salary-container">
+          <p class="data-type-label">My salary</p>
+          <p class="salary">1,234 €</p>
+          <p class="salary-type-label">net salary</p>
+        </div>
+        <div class="median-salary-container">
+          <p class="data-type-label">My salary</p>
+          <p class="salary">1,234 €</p>
+          <p class="salary-type-label">net salary</p>
+        </div>
       </div>
 
       <!--
@@ -36,15 +111,17 @@ export default {
 </script>
 
 <style scoped>
+/* ----------------- General styles ----------------- */
 .results {
-  margin-top: 97px; /* Adjust this value to ensure content starts below the navbar */
+  margin-top: 97px;
+  /* Adjust this value to ensure content starts below the navbar */
   text-align: left;
   padding-left: 20px;
 }
 
 h1 {
   font-weight: bold;
-  color: #333333;
+  color: #333;
   position: relative;
   display: inline-block;
   margin-bottom: 50px;
@@ -60,33 +137,156 @@ h1::after {
   background-color: #D9FF80;
 }
 
-.salary-container {
+/* ----------------- Filters and salaries amount container ----------------- */
+.overall-filter-section {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+/* ----------------- Filters ----------------- */
+
+.filters {
+  order: 1;
+  margin-right: 120px;
+}
+
+.filter-row {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.filter-row label {
+  width: 120px;
+  padding: 8px;
+  margin-top: 0px;
+}
+
+.filter-row .input-field {
+  flex: 1;
+  width: 100%;
+  padding: 8px;
+  border: none;
+  background-color: white;
+}
+
+.filters-title {
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+.filter-label {
+  display: block;
+  margin-top: 10px;
+  color: #333;
+}
+
+.input-field.disabled {
+  background-color: #f5F7EE;
+  color: #969694;
+  box-shadow: inset 0 0 0 1px #969694;
+}
+
+.seniority-row {
+  align-items: flex-start;
+  /* Poravnava label s gumbima */
+}
+
+.seniority-group {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+}
+
+.seniority-btn {
+  flex: 1;
+  padding: 8px;
+  border: none;
+  border-radius: 4px;
+  background-color: white;
+  cursor: pointer;
+}
+
+.seniority-btn.active {
+  background-color: white;
+  color: black;
+  border: none;
+  box-shadow: inset 0 0 0 2px black;
+}
+
+/* ----------------- Amount of salaries ----------------- */
+
+.salaries-amount-container {
+  order: 2;
+}
+
+.salaries-amount-number {
+  font-weight: 700;
+  line-height: 1;
+  margin-bottom: 10px;
+}
+
+.salaries-amount-label {
+  font-weight: 500;
+  margin-top: 0;
+  margin-bottom: 10px;
+}
+
+.salaries-amount-note {
+  font-weight: 300;
+  margin-top: 0;
+}
+
+/* ----------------- Data containers ----------------- */
+
+.overall-data-section {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 50px;
+}
+
+/* ----------------- My salary ----------------- */
+.my-salary-container {
   display: flex;
   flex-direction: column;
-  width: fit-content; /* Container width becomes text size */
+  width: fit-content;
+  /* Container width becomes text size */
 }
-  
-.label {
+
+.data-type-label {
   font-weight: bold;
   color: #333333;
   margin-bottom: 40px;
   line-height: 1;
 }
 
-.salary {
-  font-weight: bold;
+.salary-my {
+  font-weight: 600;
   color: #FF9883;
   margin: 0;
   line-height: 1;
 }
 
-.salary-label {
+.salary {
+  font-weight: 400;
   color: #333333;
-  margin-top: 5px;
-  text-align: right; /* Aligns right within the container */
+  margin: 0;
+  line-height: 1;
 }
 
-@media (min-width: 768px) { /* Large screens */
+.salary-type-label {
+  color: #333333;
+  margin-top: 5px;
+  text-align: right;
+  /* Aligns right within the container */
+}
+
+/* ----------------- Media queries ----------------- */
+
+/* Large screens */
+@media (min-width: 768px) {
+
   h1 {
     font-size: 48px;
   }
@@ -95,20 +295,59 @@ h1::after {
     height: 10px;
   }
 
-  .label {
+  /* Filters */
+  .filters {
+    min-width: 500px;
+    max-width: 600px;
+  }
+
+  .filters-title {
+    font-size: 18px;
+  }
+
+  .filter-label {
+    font-size: 18px;
+  }
+
+  .filter-row .input-field {
+    font-size: 18px;
+  }
+
+  .seniority-btn {
+    font-size: 18px;
+  }
+
+  /* Amount of salaries */
+  .salaries-amount-number {
+    font-size: 48px;
+  }
+
+  .salaries-amount-label {
     font-size: 24px;
   }
 
-  .salary {
+  .salaries-amount-note {
+    font-size: 14px;
+  }
+
+  /* Data containers */
+  .data-type-label {
+    font-size: 24px;
+  }
+
+  .salary,
+  .salary-my {
     font-size: 72px;
   }
 
-  .salary-label {
+  .salary-type-label {
     font-size: 18px;
   }
 }
 
-@media (max-width: 768px) { /* Mobile phones */
+/* Mobile phones */
+@media (max-width: 768px) {
+
   h1 {
     font-size: 24px;
   }
@@ -117,15 +356,52 @@ h1::after {
     height: 5px;
   }
 
-  .label {
+  /* Filters */
+  .filters {
+    min-width: 300px;
+    max-width: 400px;
+  }
+
+  .filters-title {
+    font-size: 12px;
+  }
+
+  .filter-label {
+    font-size: 12px;
+  }
+
+  .filter-row .input-field {
+    font-size: 12px;
+  }
+
+  .seniority-btn {
+    font-size: 12px;
+  }
+
+  /* Amount of salaries */
+  .salaries-amount-number {
+    font-size: 32px;
+  }
+
+  .salaries-amount-label {
+    font-size: 18px;
+  }
+
+  .salaries-amount-note {
+    font-size: 12px;
+  }
+
+  /* Data containers */
+  .data-type-label {
     font-size: 24px;
   }
 
-  .salary {
+  .salary,
+  .salary-my {
     font-size: 48px;
   }
 
-  .salary-label {
+  .salary-type-label {
     font-size: 12px;
   }
 }
