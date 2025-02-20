0051189ddb9d49df750a67c649ec5f589a29c4f1
Vatroslav Mileusnić <mileusnic.vatroslav@gmail.com>
2025-02-20 15:56:17 +0100

    Deleted remaining unused file and mentions of the files in index.ts.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
-    },
     {
       path: '/',
       alias: '/login',
@@ -17,22 +11,9 @@ const router = createRouter({
       component: LoginView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/results',
       name: 'results',
       component: () => import('@/views/ResultsView.vue'),
-    },
-    {
-      path: '/submission',
-      name: 'submission',
-      component: () => import('@/views/SubmissionView.vue'),
     },
   ],
 })
